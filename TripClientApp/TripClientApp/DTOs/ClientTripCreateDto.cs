738247853afbd4a21805c03a4d99e9f307359c2d
s31083 <s31083@pjwstk.edu.pl>
2025-05-31 17:42:51 +0200

    added dto validation

--- TripClientApp/TripClientApp/DTOs/ClientTripCreateDto.cs
+++ TripClientApp/TripClientApp/DTOs/ClientTripCreateDto.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TripClientApp.DTOs;
 
 public class ClientTripCreateDto
 {
+    [Required]
+    [MaxLength(120)]
     public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(120)]
     public string LastName { get; set; }
+    
+    [Required]
+    [MaxLength(120)]
     public string Email { get; set; }
+    
+    [Required]
+    [MaxLength(120)]
     public string Telephone { get; set; }
+    
+    [Required]
+    [MaxLength(120)]
     public string Pesel { get; set; }
     public string? PaymentDate { get; set; }
     /*
